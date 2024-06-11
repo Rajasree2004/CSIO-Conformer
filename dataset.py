3200d528a0580c7494c9f4d16c47d41c35d531c1
Rajasree2004 <rajasree.selva@gmail.com>
2024-06-11 17:06:15 +0530

    implemented training function for conformer

--- dataset.py
+++ dataset.py
@@ -57,6 +57,9 @@ def build_dataset(is_train, args):
     if args.data_set == 'CIFAR':
         dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform)
         nb_classes = 100
+    elif args.data_set == 'RGB_images':
+        dataset = datasets.CIFAR100(args.data_path, train=is_train, transform=transform)
+        nb_classes = 100
     elif args.data_set == 'CIFAR10':
         dataset = datasets.CIFAR10(args.data_path, train=is_train, transform=transform)
         nb_classes = 10
